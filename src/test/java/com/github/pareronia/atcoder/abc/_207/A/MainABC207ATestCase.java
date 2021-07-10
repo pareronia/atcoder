ba2d049b8e17c30c0f7ffae41cfbd10936feb6ed
pareronia <49491686+pareronia@users.noreply.github.com>
2021-07-10 11:13:37 +0200

    ABC Round 207 Problems A, B test cases

---
+++ src/test/java/com/github/pareronia/atcoder/abc/_207/A/MainABC207ATestCase.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.atcoder.abc._207.A;
+
+import com.github.pareronia.atcoder.MainTestBase;
+
+public class MainABC207ATestCase extends MainTestBase<Main> {
+
+    public MainABC207ATestCase() {
+        super(Main.class);
+    }
+}
---
+++ src/test/java/com/github/pareronia/atcoder/abc/_207/B/MainABC207BTestCase.java
@@ -0,0 +1,10 @@
+package com.github.pareronia.atcoder.abc._207.B;
+
+import com.github.pareronia.atcoder.MainTestBase;
+
+public class MainABC207BTestCase extends MainTestBase<Main>{
+
+    public MainABC207BTestCase() {
+        super(Main.class);
+    }
+}
