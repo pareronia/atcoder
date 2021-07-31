9f00ea5c2531bc6278610225883f5ae4e6c428a1
pareronia <49491686+pareronia@users.noreply.github.com>
2021-07-31 19:50:25 +0200

    ABC Round 212 Problem E - TLE

---
+++ src/main/java/com/github/pareronia/atcoder/abc/_212/e/Main.java
@@ -0,0 +1,225 @@
+package com.github.pareronia.atcoder.abc._212.e;
+
+import static java.util.Arrays.asList;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+/**
+ * E - Safety Journey
+ * @see <a href="https://atcoder.jp/contests/abc212/tasks/abc212_e">https://atcoder.jp/contests/abc212/tasks/abc212_e</a>
+ */
+public class Main {
+    
+    public static final long MOD = 998_244_353;
+
+    private final boolean sample;
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Main(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.sample = sample;
+        this.in = in;
+        this.out = out;
+    }
+    
+    @SuppressWarnings("unused")
+    private void log(final Supplier<Object> supplier) {
+        if (!sample) {
+            return;
+        }
+        System.out.println(supplier.get());
+    }
+    
+    private long bfs(final int n, final int start, final int k, final List<Pair<Integer, Integer>> unu) {
+        final Deque<Pair<Integer, Integer>> queue = new ArrayDeque<>();
+        long cnt = 0;
+        queue.add(Pair.of(start, 1));
+        while (!queue.isEmpty()) {
+            final Pair<Integer,Integer> trip = queue.poll();
+            final Integer last = trip.getOne();
+            final Integer size = trip.getTwo();
+            if (size == k + 1) {
+                if (start == last) {
+                    cnt = (cnt + 1) % MOD;
+                }
+                continue;
+            }
+            for (int j = 1; j <= n; j++) {
+                if (j == last) {
+                    continue;
+                }
+                if (unu.contains(Pair.of(j, last))) {
+                    continue;
+                }
+                final Pair<Integer, Integer> newTrip = Pair.of(j, size + 1);
+                queue.add(newTrip);
+            }
+        }
+        return cnt;
+    }
+    
+    private Result<?> handleTestCase(final Scanner sc, final Integer i) {
+        final int n = sc.nextInt();
+        final int m = sc.nextInt();
+        final int k = sc.nextInt();
+        final List<Pair<Integer, Integer>> unu = new ArrayList<>(2 * m);
+        for (int j = 0; j < m; j++) {
+            final int u = sc.nextInt();
+            final int v = sc.nextInt();
+            unu.add(Pair.of(u, v));
+            unu.add(Pair.of(v, u));
+        }
+        long ans = 0;
+        ans += (bfs(n, 1, k, unu) % MOD);
+        final List<Long> tmp = List.of(ans);
+        log(() -> tmp);
+        return new Result<>(i, List.of(ans));
+    }
+    
+    private void output(final List<Result<?>> results) {
+        results.forEach(r -> this.out.println(
+                r.values.stream().map(Object::toString).collect(joining(" "))));
+    }
+    
+    public void solve() throws IOException {
+        try (final Scanner sc = new Scanner(new InputStreamReader(this.in))) {
+            final int numberOfTestCases;
+            if (sample) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            final List<Result<?>> results =
+                    Stream.iterate(1, i -> i <= numberOfTestCases, i -> i + 1)
+                            .map(i -> handleTestCase(sc, i))
+                            .collect(toList());
+            output(results);
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Main.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Main(sample, is, out).solve();
+    	
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Main.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+
+    private static boolean isSample() {
+        return "sample".equals(System.getProperty("atcoder"));
+    }
+    
+    private static final class Result<T> {
+        private final List<T> values;
+
+        public Result(final int caseNumber, final List<T> values) {
+            this.values = values;
+        }
+    }
+    
+    @SuppressWarnings("unused")
+    private static final class Pair<L, R> {
+        private final L one;
+        private final R two;
+
+        private Pair(final L one, final R two) {
+            this.one = one;
+            this.two = two;
+        }
+
+        public static <L, R> Pair<L, R> of(final L one, final R two) {
+            return new Pair<>(one, two);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(one, two);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public boolean equals(final Object obj) {
+            if (this == obj) {
+                return true;
+            }
+            if (obj == null) {
+                return false;
+            }
+            if (getClass() != obj.getClass()) {
+                return false;
+            }
+            final Pair<Integer, Integer> other = (Pair<Integer, Integer>) obj;
+            return Objects.equals(one, other.one) && Objects.equals(two, other.two);
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder builder = new StringBuilder();
+            builder.append("Pair [one=").append(one).append(", two=").append(two).append("]");
+            return builder.toString();
+        }
+
+        public L getOne() {
+            return one;
+        }
+        
+        public R getTwo() {
+            return two;
+        }
+    }
+}
