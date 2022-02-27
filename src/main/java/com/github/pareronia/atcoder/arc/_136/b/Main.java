82b22cd08b991394704e27ebeccdd3a8fc77595b
pareronia <49491686+pareronia@users.noreply.github.com>
2022-02-27 17:43:50 +0100

    [TLE/RE] ARC Round 136 Problem B

---
+++ src/main/java/com/github/pareronia/atcoder/arc/_136/b/Main.java
@@ -0,0 +1,177 @@
+package com.github.pareronia.atcoder.arc._136.b;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+/**
+ * B - Triple Shift
+ * @see <a href="https://atcoder.jp/contests/arc136/tasks/arc136_b">https://atcoder.jp/contests/arc136/tasks/arc136_b</a>
+ */
+public class Main {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Main(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final FastScanner sc, final Integer i) {
+        final int n = sc.nextInt();
+        final int[] a = sc.nextIntArray(n);
+        final int[] b = sc.nextIntArray(n);
+        String ans = "No";
+        final Deque<int[]> q = new ArrayDeque<>();
+        q.add(a);
+        final Set<int[]> seen = new HashSet<>();
+        while (!q.isEmpty()) {
+            final int[] c = q.poll();
+            if (Arrays.equals(c, b)) {
+                ans = "Yes";
+                break;
+            }
+            seen.add(c);
+            for (int j = 0; j + 2 < n; j++) {
+                final int[] nc = Arrays.copyOf(c, n);
+                final int x = nc[j];
+                final int y = nc[j + 1];
+                final int z = nc[j + 2];
+                nc[j] = z;
+                nc[j + 1] = x;
+                nc[j + 2] = y;
+                if (!seen.stream().anyMatch(s -> Arrays.equals(s, nc))) {
+                    q.add(nc);
+                }
+            }
+        }
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases;
+            if (isSample()) {
+                numberOfTestCases = sc.nextInt();
+            } else {
+                numberOfTestCases = 1;
+            }
+            for (int i = 1; i <= numberOfTestCases; i++) {
+                handleTestCase(sc, i);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Main.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Main(sample, is, out).solve();
+    	
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Main.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("atcoder"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = nextInt();
+            }
+            return a;
+        }
+        
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
