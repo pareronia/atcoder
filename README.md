ca18060f3d6f3e24f969fae920d70f2e4e073f76
pareronia <49491686+pareronia@users.noreply.github.com>
2022-03-26 15:29:10 +0100

    ABC Round 245 Problem C

--- README.md
+++ README.md
@@ -77,6 +77,7 @@
 | ABC244_E | [King Bombee](https://atcoder.jp/contests/abc244/tasks/abc244_e) | [Java](src/main/java/com/github/pareronia/atcoder/abc/_244/e/Main.java) |
 | ABC245_A | [Good morning](https://atcoder.jp/contests/abc245/tasks/abc245_a) | [Java](src/main/java/com/github/pareronia/atcoder/abc/_245/a/Main.java) |
 | ABC245_B | [Mex](https://atcoder.jp/contests/abc245/tasks/abc245_b) | [Java](src/main/java/com/github/pareronia/atcoder/abc/_245/b/Main.java) |
+| ABC245_C | [Choose Elements](https://atcoder.jp/contests/abc245/tasks/abc245_c) | [Java](src/main/java/com/github/pareronia/atcoder/abc/_245/c/Main.java) |
 | ARC123_A | [Arithmetic Sequence](https://atcoder.jp/contests/arc123/tasks/arc123_a) | [Java](src/main/java/com/github/pareronia/atcoder/arc/_123/a/Main.java) |
 | ARC136_A | [A ↔ BB](https://atcoder.jp/contests/arc136/tasks/arc136_a) | [Java](src/main/java/com/github/pareronia/atcoder/arc/_136/a/Main.java) |
 | ARC136_B | [Triple Shift](https://atcoder.jp/contests/arc136/tasks/arc136_b) | [Java](src/main/java/com/github/pareronia/atcoder/arc/_136/b/Main.java) |
---
+++ src/main/java/com/github/pareronia/atcoder/abc/_245/c/Main.java
@@ -0,0 +1,155 @@
+package com.github.pareronia.atcoder.abc._245.c;
+
+import static java.util.Arrays.asList;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.Closeable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * C - Choose Elements
+ * @see <a href="https://atcoder.jp/contests/abc245/tasks/abc245_c">https://atcoder.jp/contests/abc245/tasks/abc245_c</a>
+ */
+public class Main {
+
+    private final InputStream in;
+    private final PrintStream out;
+    
+    public Main(
+            final Boolean sample, final InputStream in, final PrintStream out) {
+        this.in = in;
+        this.out = out;
+    }
+    
+    private void handleTestCase(final FastScanner sc, final Integer i) {
+        final int n = sc.nextInt();
+        final int k = sc.nextInt();
+        final int[] a = sc.nextIntArray(n);
+        final int[] b = sc.nextIntArray(n);
+        final boolean[] dpa = new boolean[n];
+        final boolean[] dpb = new boolean[n];
+        dpa[0] = true;
+        dpb[0] = true;
+        for (int j = 1; j < n; j++) {
+            dpa[j] = (dpa[j - 1] && Math.abs(a[j - 1] - a[j]) <= k
+                    || dpb[j - 1] && Math.abs(b[j - 1] - a[j]) <= k);
+            dpb[j] = (dpa[j - 1] && Math.abs(a[j - 1] - b[j]) <= k
+                    || dpb[j - 1] && Math.abs(b[j - 1] - b[j]) <= k);
+        }
+        final String ans = dpa[n - 1] || dpb[n - 1] ? "Yes" : "No";
+        this.out.println(ans);
+    }
+    
+    public void solve() {
+        try (final FastScanner sc = new FastScanner(this.in)) {
+            final int numberOfTestCases = isSample() ? sc.nextInt() : 1;
+            for (int i = 1; i <= numberOfTestCases; i++) {
+                handleTestCase(sc, i);
+            }
+        }
+    }
+
+    public static void main(final String[] args) throws IOException, URISyntaxException {
+        final boolean sample = isSample();
+        final InputStream is;
+        final PrintStream out;
+        final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        long timerStart = 0;
+        if (sample) {
+            is = Main.class.getResourceAsStream("sample.in");
+            out = new PrintStream(baos, true);
+            timerStart = System.nanoTime();
+        } else {
+            is = System.in;
+            out = System.out;
+        }
+        
+        new Main(sample, is, out).solve();
+    	
+        if (sample) {
+            final long timeSpent = (System.nanoTime() - timerStart) / 1_000;
+            final double time;
+            final String unit;
+            if (timeSpent < 1_000) {
+                time = timeSpent;
+                unit = "µs";
+            } else if (timeSpent < 1_000_000) {
+                time = timeSpent / 1_000.0;
+                unit = "ms";
+            } else {
+                time = timeSpent / 1_000_000.0;
+                unit = "s";
+            }
+            final Path path
+                    = Paths.get(Main.class.getResource("sample.out").toURI());
+            final List<String> expected = Files.readAllLines(path);
+            final List<String> actual = asList(baos.toString().split("\\r?\\n"));
+            if (!expected.equals(actual)) {
+                throw new AssertionError(String.format(
+                        "Expected %s, got %s", expected, actual));
+            }
+            actual.forEach(System.out::println);
+            System.out.println(String.format("took: %.3f %s", time, unit));
+        }
+    }
+
+    private static boolean isSample() {
+        try {
+            return "sample".equals(System.getProperty("atcoder"));
+        } catch (final SecurityException e) {
+            return false;
+        }
+    }
+
+    private static final class FastScanner implements Closeable {
+        private final BufferedReader br;
+        private StringTokenizer st;
+        
+        public FastScanner(final InputStream in) {
+            this.br = new BufferedReader(new InputStreamReader(in));
+            st = new StringTokenizer("");
+        }
+        
+        public String next() {
+            while (!st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (final IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return st.nextToken();
+        }
+    
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        public int[] nextIntArray(final int n) {
+            final int[] a = new int[n];
+            for (int j = 0; j < n; j++) {
+                a[j] = nextInt();
+            }
+            return a;
+        }
+
+        @Override
+        public void close() {
+            try {
+                this.br.close();
+            } catch (final IOException e) {
+                // ignore
+            }
+        }
+    }
+}
